b19b5f4b110fcfe5587831b3065f04d546190cc0
Yeghishe <85505705+nexo8937@users.noreply.github.com>
2024-05-19 15:36:41 +0400

    Update module.tf

--- terraform-eks/module.tf
+++ terraform-eks/module.tf
@@ -1,9 +1,15 @@
 module "eks" {
     source                   = "github.com/nexo8937/terraform-modules//eks"
     app                      = "Brain-Scale"
+    #EKS
     cluster_version          = "1.29"
-    public_subnets          = data.terraform_remote_state.backend.outputs.public_subnets
+    public_subnets           = data.terraform_remote_state.backend.outputs.public_subnets
     private_subnets          = data.terraform_remote_state.backend.outputs.private_subnets
+    #NODEGROUP
+    instance_type            = "t3.small"
+    desired_size             = "1"
+    max_size                 = "2"
+    min_size                 = "1"             
 }
 
 #remote-state-data
